eb96164170a9f38c737da43458d68f80e79f4981
nologicexe <nologicex@gmail.com>
2021-01-26 12:35:43 +0300

    v1.0b

--- speech-analyzer/formants.py
+++ speech-analyzer/formants.py
@@ -9,8 +9,9 @@ import scipy
 from os import path
 import math
 import functions as func
+import json
 
-def getFormants(filename, ifLim, limit):
+def getFormants(filename, ifLim, limit, savefile=''):
     sRate, data = func.get_data(filename, ifLim, limit)
     frame_size = sRate//8
     steps = len(data)//frame_size
@@ -24,11 +25,15 @@ def getFormants(filename, ifLim, limit):
         "formantFreq": formantFreq.tolist(),
         "formantValue": formantValue.tolist()
     }
+    if (savefile != ''):
+        with open(savefile, 'w') as json_file:
+            json.dump(output, json_file)
+
     return output
 
 if __name__ == "__main__":
     filename, limit, ifLim = func.get_args()
-    output = getFormants(filename, ifLim, limit)
+    output = getFormants(filename, ifLim, limit, savefile='test.json')
     goodness = output["goodness"]
     formantFreq = output["formantFreq"]
     formantValue = output["formantValue"]
--- speech-analyzer/speechSpeed.py
+++ speech-analyzer/speechSpeed.py
@@ -3,7 +3,7 @@ import numpy as np
 import functions as func
 import json
 
-def getSpeechSpeed(filename, ifLim, limit):
+def getSpeechSpeed(filename, ifLim, limit, savefile=''):
     sRate, data = func.get_data(filename, ifLim, limit)
     frame_size = sRate//8
     steps = len(data)//frame_size
@@ -28,16 +28,20 @@ def getSpeechSpeed(filename, ifLim, limit):
     silence /= stepsInSec
     outputY = words/silence
     outputX = np.arange(fullIntervals+1)*interval
-    
+
     output = {
         'x': outputX.tolist(),
         'y': outputY.tolist()
     }
+    if (savefile != ''):
+        with open(savefile, 'w') as json_file:
+            json.dump(output, json_file)
+    
     return output
 
 if __name__ == '__main__':
     filename, limit, ifLim = func.get_args()
-    output = getSpeechSpeed(filename, ifLim, limit)
+    output = getSpeechSpeed(filename, ifLim, limit, 'test.json')
 
     plt.plot(output['x'], output['y'])
     plt.title("Words per second")
