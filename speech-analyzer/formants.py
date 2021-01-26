77d6d44c3e206de2652e042a2a3e434106339523
nologicexe <nologicex@gmail.com>
2021-01-26 12:09:48 +0300

    v1.0b

--- speech-analyzer/formants.py
+++ speech-analyzer/formants.py
@@ -10,47 +10,34 @@ from os import path
 import math
 import functions as func
 
-#load and calculate formants
-filename, limit, ifLim = func.get_args()
-sRate, data = func.get_data(filename, ifLim, limit)
-frame_size = sRate//8
-steps = len(data)//frame_size
+def getFormants(filename, ifLim, limit):
+    sRate, data = func.get_data(filename, ifLim, limit)
+    frame_size = sRate//8
+    steps = len(data)//frame_size
 
-#plot audio
-# for i in range(steps):
-#     plt.plot(data[i*frame_size:(i+1)*frame_size])
-#     plt.show()
+    spectrogram, X, timeArray = func.get_spectrogram(sRate, data, frame_size, steps)
+    goodness, goodSum, signalMins, SpectrumMaxes = func.get_good(data, steps, frame_size, spectrogram)
+    formantID, formantFreq, formantValue = func.get_formants(spectrogram, X, steps)
 
-spectrogram, X, timeArray = func.get_spectrogram(sRate, data, frame_size, steps)
-goodness, goodSum, signalMins, SpectrumMaxes = func.get_good(data, steps, frame_size, spectrogram)
-formantID, formantFreq, formantValue = func.get_formants(spectrogram, X, steps)
+    output = {
+        "goodness": goodness.tolist(),
+        "formantFreq": formantFreq.tolist(),
+        "formantValue": formantValue.tolist()
+    }
+    return output
 
-#save formant data to formants.npy
-save_array = np.array([goodness, formantFreq, formantValue])
-np.save("formants.npy", save_array)
+if __name__ == "__main__":
+    filename, limit, ifLim = func.get_args()
+    output = getFormants(filename, ifLim, limit)
+    goodness = output["goodness"]
+    formantFreq = output["formantFreq"]
+    formantValue = output["formantValue"]
 
-#plot formant frequency
-x_freq = np.array([i for i in range(len(goodness)) if goodness[i]])
-y_freq = np.array([formantFreq[i] for i in range(len(goodness)) if goodness[i]])
-bad_x_freq = np.array([i for i in range(len(goodness)) if not goodness[i]])
-bad_y_freq = np.array([formantFreq[i] for i in range(len(goodness)) if not goodness[i]])
-plt.plot(np.arange(len(formantFreq))/8, formantFreq, '-', color=(0.8, 0.8, 0.8))
-plt.plot(x_freq/8, y_freq, '.')
-#plt.plot(bad_x_freq/8, bad_y_freq, 'r.')
-plt.show()
-
-#plot spectrogram for every chunk
-for i in range(len(spectrogram)):
-    plt.plot(X, spectrogram[i])
-    plt.title(str(i))
-    plt.ylim(0, 1e7)
-    plt.plot(formantFreq[i], formantValue[i], 'o', color=(1.0, 0.0, 0.0))
+    x_freq = np.array([i for i in range(len(goodness)) if goodness[i]])
+    y_freq = np.array([formantFreq[i] for i in range(len(goodness)) if goodness[i]])
+    bad_x_freq = np.array([i for i in range(len(goodness)) if not goodness[i]])
+    bad_y_freq = np.array([formantFreq[i] for i in range(len(goodness)) if not goodness[i]])
+    plt.plot(np.arange(len(formantFreq))/8, formantFreq, '-', color=(0.8, 0.8, 0.8))
+    plt.plot(x_freq/8, y_freq, '.')
+    #plt.plot(bad_x_freq/8, bad_y_freq, 'r.')
     plt.show()
-
-# plt.plot(goodness)
-# plt.show()
-# func.plot_bend(data, steps, frame_size, signalMins, SpectrumMaxes)
-# func.plot_freq_response(formantFreq, formantValue, goodness)
-# func.plot_spectrogram(timeArray, X, spectrogram, formantID)
-# func.postprocessing(spectrogram, X, data, frame_size)
-#asdasdas
